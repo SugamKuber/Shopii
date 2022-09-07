eda34a6541a7bca6952f5a2e54da66e2de47947c
sugam7842692@gmail.com <sugamkuber@gmail.com>
2022-09-07 18:56:26 +0530

    Committwo

---
+++ components/sidebar.js
@@ -0,0 +1,10 @@
+import { ConnectButton } from 'web3uikit'
+
+export default function Sidebar() {
+    return (
+        <>
+            <div>hiiii</div>
+            <ConnectButton />
+        </>
+    )
+}
